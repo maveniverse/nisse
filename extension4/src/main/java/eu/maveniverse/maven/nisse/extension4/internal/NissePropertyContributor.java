9ab266bd3e220c4f961df8fa3a9a23c17e6a6ffe
Tamas Cservenak <tamas@cservenak.net>
2024-12-16 17:24:52 +0100

    Extensiob for Maven4
    Fixes #36

---
+++ extension4/src/main/java/eu/maveniverse/maven/nisse/extension4/internal/NissePropertyContributor.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+package eu.maveniverse.maven.nisse.extension4.internal;
+
+import static java.util.Objects.requireNonNull;
+
+import eu.maveniverse.maven.nisse.core.NisseConfiguration;
+import eu.maveniverse.maven.nisse.core.NisseManager;
+import eu.maveniverse.maven.nisse.core.Version;
+import eu.maveniverse.maven.nisse.core.internal.SimpleNisseConfiguration;
+import java.util.Map;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import org.apache.maven.api.ProtoSession;
+import org.apache.maven.api.spi.PropertyContributor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+@Named
+final class NissePropertyContributor implements PropertyContributor {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final NisseManager nisseManager;
+
+    @Inject
+    public NissePropertyContributor(NisseManager nisseManager) {
+        this.nisseManager = requireNonNull(nisseManager, "nisseManager");
+    }
+
+    @Override
+    public Map<String, String> contribute(ProtoSession protoSession) {
+        logger.info("Maveniverse Nisse {} loaded", Version.version());
+        // create properties and push what we got into CLI user properties
+        NisseConfiguration configuration = SimpleNisseConfiguration.builder()
+                .withSystemProperties(protoSession.getSystemProperties())
+                .withUserProperties(protoSession.getUserProperties())
+                .withCurrentWorkingDirectory(protoSession.getTopDirectory())
+                .withSessionRootDirectory(protoSession.getRootDirectory())
+                .build();
+        Map<String, String> nisseProperties = nisseManager.createProperties(configuration);
+        logger.info("Nisse injecting {} properties into User Properties", nisseProperties.size());
+        return nisseProperties;
+    }
+}
---
+++ it/extension4-its/src/it/jgit-dynamicVersion/verify.groovy
@@ -0,0 +1,3 @@
+File buildLog = new File( basedir, 'build.log' )
+assert buildLog.exists()
+assert buildLog.text.contains ('[INFO]  * ${nisse.jgit.dynamicVersion}=0.0.1')
